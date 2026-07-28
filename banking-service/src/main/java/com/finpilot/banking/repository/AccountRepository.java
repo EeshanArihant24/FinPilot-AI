c544bbbb97d6954b30fcce0e598a374e5cd2faf0
Aryan0609 <mankararyna0609@gmail.com>
2026-07-28 11:26:19 +0530

    Backend issue fixed

--- banking-service/src/main/java/com/finpilot/banking/repository/AccountRepository.java
+++ banking-service/src/main/java/com/finpilot/banking/repository/AccountRepository.java
@@ -2,12 +2,18 @@ package com.finpilot.banking.repository;
 
 import com.finpilot.banking.entity.Account;
 import com.finpilot.banking.entity.User;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Account> findById(Long id);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Account> findByAccountNumber(String accountNumber);
 
     Optional<Account> findByUser(User user);
